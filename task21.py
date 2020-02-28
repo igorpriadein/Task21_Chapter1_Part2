ac91481ed130d98bc52c8dc13b5341afed804361
igorpriadein <iga2456@gmail.com>
2020-02-28 18:51:43 +0600

    Task21 Part2

---
+++ task21.py
@@ -0,0 +1,9 @@
+# Напишите функцию которая будет суммировать введенные три случайные цифры.
+a = int(input('a='))
+b = int(input('b='))
+c = int(input('c='))
+def sum (a, b, c):
+
+    summa = a+b+c
+    print(summa)
+sum(a, b, c)
